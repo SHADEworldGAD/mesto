2df65d422d03cc1e3f52e26b2b3f377b988d84ff
SHADEworldGAD <shadeworld@live.ru>
2022-06-25 16:02:24 +0300

    исправление HTML и CSS после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,39 @@
 let popupElement = document.querySelector('.popup');
 let popupButtonCloseElement = popupElement.querySelector('.popup__button-close');
 let profileElement = document.querySelector('.profile');
-let profileButtonAddElement = profileElement.querySelector('.profile__button-add');
+let profileButtonEditElement = profileElement.querySelector('.profile__button-edit');
 
 
 function openPopup() {
-  popupElement.classList.add('popup__opened');
+  popupElement.classList.add('popup_opened');
 }
 
-profileButtonAddElement.addEventListener('click', openPopup);
+profileButtonEditElement.addEventListener('click', openPopup);
 
 
 function closePopup() {
-  popupElement.classList.remove('popup__opened');
+  popupElement.classList.remove('popup_opened');
 }
 
 popupButtonCloseElement.addEventListener('click', closePopup);
 
 
-let popupOverlay = document.querySelector('.popup__overlay');
+// let popupOverlay = document.querySelector('.popup__overlay');
 
-function closePopupByClickOnOverlay() {
-  popupElement.classList.remove('popup__opened');
-}
+// function closePopupByClickOnOverlay() {
+//   popupElement.classList.remove('popup_opened');
+// }
 
-popupOverlay.addEventListener('click', closePopupByClickOnOverlay);
+// popupOverlay.addEventListener('click', closePopupByClickOnOverlay);
 
-let popupButtonSaveElement = popupElement.querySelector('.popup__button-save');
-popupButtonSaveElement.addEventListener('click', closePopup);
+// let popupButtonSaveElement = popupElement.querySelector('.popup__button-save');
+// popupButtonSaveElement.addEventListener('click', closePopup);
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__content');
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__name-input');
-let jobInput = formElement.querySelector('.popup__aboutme-input');
+let nameInput = formElement.querySelector('.popup__input-name');
+let jobInput = formElement.querySelector('.popup__input-aboutme');
 
 let nameProfileElement = profileElement.querySelector('.profile__title');
 let aboutmeProfileElement = profileElement.querySelector('.profile__subtitle');
@@ -46,13 +46,17 @@ function formSubmitHandler(evt) {
   // О том, как это делать, расскажем позже.
 
   // Получите значение полей jobInput и nameInput из свойства value
-  nameInput.value;
-  jobInput.value;
+  // nameInput.value;
+  // jobInput.value;
 
   // Выберите элементы, куда должны быть вставлены значения полей
   // Вставьте новые значения с помощью textContent
   nameProfileElement.textContent = nameInput.value;
   aboutmeProfileElement.textContent = jobInput.value;
+
+
+
+
 }
 
 // Прикрепляем обработчик к форме:
